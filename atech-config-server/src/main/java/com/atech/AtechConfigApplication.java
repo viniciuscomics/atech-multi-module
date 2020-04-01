ffa58e1822fd97740c6e0287fa1c2201f8bdcb86
Vinicius Costa <42116742+viniciuscomics@users.noreply.github.com>
2020-04-01 00:19:42 -0300

    Add module atech-config-server

---
+++ atech-config-server/src/main/java/com/atech/AtechConfigApplication.java
@@ -0,0 +1,15 @@
+package com.atech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class AtechConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtechConfigApplication.class, args);
+	}
+
+}
