3f28705d5cc07a1dc09fdd3f3f25469a682a8fa1
Vinicius Costa <42116742+viniciuscomics@users.noreply.github.com>
2020-04-01 14:55:22 -0300

    Script to compile and run app java in docker

---
+++ atech-auth-server/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+tag=$(git symbolic-ref HEAD 2>/dev/null | awk -F '/' '{print $NF}')
+mvn clean package docker:build -DdockerImageTags=$tag
+docker run -p 8083:8083 --rm --network service -ti atech-auth-server:$tag
