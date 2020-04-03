b3727377b194c4d2d730139366b6f572d982fcd4
root <root@vinicius-Inspiron-3583>
2020-04-03 10:18:47 -0300

    Correction in the deploy script

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 
 echo "Criando banco para testes"
 
+docker stop authserverdbteste
+
+docker rm authserverdbteste
+
 docker run -p 5665:5432 --name authserverdbteste -e POSTGRES_USER=atech -e POSTGRES_PASSWORD=atech -e POSTGRES_DB=authserverdbteste -d postgres:10.5-alpine
 
 echo "Iniciando compilacao dos servicos..."
