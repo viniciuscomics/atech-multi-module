166b6ada1d562f17bc0ca833e6788802e4ee452f
root <root@vinicius-Inspiron-3583>
2020-04-03 10:11:34 -0300

    Add script to compile, run tests and run docker compose.

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando banco para testes"
+
+docker run -p 5665:5432 --name authserverdbteste -e POSTGRES_USER=atech -e POSTGRES_PASSWORD=atech -e POSTGRES_DB=authserverdbteste -d postgres:10.5-alpine
+
+echo "Iniciando compilacao dos servicos..."
+
+mvn clean package
+
+echo "Apagando banco para testes"
+
+docker stop authserverdbteste
+
+docker rm authserverdbteste
+
+echo "Iniciando o docker compose..."
+
+docker-compose up --build
