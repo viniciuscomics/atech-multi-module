f51866166fbc1da860e2ebacedc82f020eaa1b36
Vinicius Costa <42116742+viniciuscomics@users.noreply.github.com>
2020-04-06 13:30:53 -0300

    Delete run.sh

--- atech-config-server/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-tag=$(git symbolic-ref HEAD 2>/dev/null | awk -F '/' '{print $NF}')
-mvn clean package docker:build -DdockerImageTags=$tag
-docker run -p 8081:8081 --rm --network service -ti atech-config-server:$tag
